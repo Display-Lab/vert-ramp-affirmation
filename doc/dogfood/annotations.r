5e8a9b6108fa7b87881b7f1892435cdda545a97c
Colin Gross <grosscol@gmail.com>
2019-03-06 10:20:36 -0500

    Initial components for VA pipeline example.

---
+++ doc/dogfood/annotations.r
@@ -0,0 +1,51 @@
+library(dplyr, warn.conflicts = FALSE)
+library(utils)
+
+# Annotations for VA example
+
+## Helper functions
+eval_obs_paucity <- function(doc, ndoc){
+
+  c_obs <- sum(doc, ndoc)
+  c_last <- sum(, dplyr::last(ndoc))
+
+  if( c_obs < 12 & 
+      dplyr::last(ndoc) < 6 &
+      dplyr::last(doc) < 6 ){
+    return(TRUE)
+  }
+
+  return(FALSE)
+}
+
+eval_trend <- function(doc, ndoc){
+  ratio <- doc/ndoc
+  is_tail_ascending <- !is.unsorted(utils::tail(ratio,3), strictly=T)
+  return(is_tail_ascending)
+}
+
+eval_negative_gap <- function(doc, ndoc){
+  ratio <- doc/ndoc
+  return(dplyr::last(ratio) < 0.9)
+}
+
+## Annotation functions
+annotate_obs_paucity <- function(data, col_spec){
+  data %>%
+    group_by(sta6a) %>%
+    arrange(report_month) %>%
+    summarize(obs_paucity = eval_obs_paucity(documented, not_documented))
+}
+
+annotate_negative_gap <- function(data, col_spec){
+  data %>% group_by(sta6a) %>%
+    dplyr::filter(report_month == max(report_month)) %>%
+    summarize(negative_gap = eval_gap(documented, not_documented))
+}
+
+annotate_negative_trend <- function(data, col_spec){
+  data %>%
+    group_by(sta6a) %>%
+    arrange(report_month) %>%
+    summarize(negative_trend = eval_negative_trend(performance))
+}
