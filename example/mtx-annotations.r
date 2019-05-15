c317246942314afb4c6553d373cde9a61eb48478
Colin Gross <grosscol@gmail.com>
2019-05-15 16:32:54 -0400

    Stub VA example runtime requirements. Correct mtx annotations and setup.

--- example/mtx-annotations.r
+++ example/mtx-annotations.r
@@ -11,9 +11,11 @@ library(stats)
 ############
 
 # Calculate the achievable benchmark
-setup <- function(data, spek, t_env){
-  # stub
-  t_env$achievable_benchmark <- 10
+setup_cache <- function(data, spek){
+  cache <- list()
+  cache$achievable_benchmark <- calc_achievable_benchmark(data, spek)
+
+  return(cache)
 }
 
 ####################
@@ -28,12 +30,12 @@ calc_achievable_benchmark <- function(data, spek){
 
   # Get a list of unique performer ids
   unique_performers <- unique(data[[id_colname]])
-  
+
   # Sort out how many performers constitute 10% of unique
   num_top_performers <- floor(length(unique_performers)/10)
 
   # Guard against edge case of < 10 unique performers
-  if(ten_percent == 0) { ten_percent <- 1 }
+  if(num_top_performers == 0) { num_top_performers <- 1 }
 
   # Make a symbol of the numerator and denominator columns
   denom_col_sym <- rlang::sym(denom_colname)
@@ -50,7 +52,44 @@ calc_achievable_benchmark <- function(data, spek){
 
   #Median score of top 10 performers
   return(stats::median(top_means))
+}
 
+annotate_negative_gap <- function(data, col_spec){
+  # Hardcode column names.  Extract from spek in subsequent versions.
+  id_colname <- 'id'
+  denom_colname <- 'total_scripts'
+  numer_colname <- 'high_dose_scripts'
+  time_colname <- 'period'
+  # Make a symbol of the numerator and denominator columns
+  denom_col_sym <- rlang::sym(denom_colname)
+  numer_col_sym <- rlang::sym(numer_colname)
+  id_col_sym <- rlang::sym(id_colname)
+  time_col_sym <- rlang::sym(time_colname)
+
+  data %>%
+    group_by(!!id_col_sym) %>%
+    mutate(rate=!!numer_col_sym / !!denom_col_sym) %>%
+    dplyr::filter(!!time_col_sym == max(!!time_col_sym)) %>%
+    summarize(negative_gap = rate < cache$achievable_benchmark)
+}
+
+annotate_positive_gap <- function(data, col_spec){
+  # Hardcode column names.  Extract from spek in subsequent versions.
+  id_colname <- 'id'
+  denom_colname <- 'total_scripts'
+  numer_colname <- 'high_dose_scripts'
+  time_colname <- 'period'
+  # Make a symbol of the numerator and denominator columns
+  denom_col_sym <- rlang::sym(denom_colname)
+  numer_col_sym <- rlang::sym(numer_colname)
+  id_col_sym <- rlang::sym(id_colname)
+  time_col_sym <- rlang::sym(time_colname)
+
+  data %>%
+    group_by(!!id_col_sym) %>%
+    mutate(rate=!!numer_col_sym / !!denom_col_sym) %>%
+    dplyr::filter(!!time_col_sym == max(!!time_col_sym)) %>%
+    summarize(negative_gap = rate > cache$achievable_benchmark)
 }
 
 
--- example/va-annotations.r
+++ example/va-annotations.r
@@ -2,12 +2,16 @@ library(dplyr, warn.conflicts = FALSE)
 library(utils)
 
 # Annotations for VA example
+#   Hardcoding column names currently.  Subsequent iterations are expected to take advantage of
+# column use and name specification from spek.
+
+setup <- function(data, spec){
+  cat(paste("\nSETUP\n", names(data), "\n\n"), file=stderr())
+}
 
 ## Helper functions
 eval_obs_paucity <- function(doc, ndoc){
-
   c_obs <- sum(doc, ndoc)
-  c_last <- sum(, dplyr::last(ndoc))
 
   if( c_obs < 12 & 
       dplyr::last(ndoc) < 6 &
@@ -18,7 +22,7 @@ eval_obs_paucity <- function(doc, ndoc){
   return(FALSE)
 }
 
-eval_trend <- function(doc, ndoc){
+eval_negative_trend <- function(doc, ndoc){
   ratio <- doc/ndoc
   is_tail_ascending <- !is.unsorted(utils::tail(ratio,3), strictly=T)
   return(is_tail_ascending)
@@ -32,20 +36,21 @@ eval_negative_gap <- function(doc, ndoc){
 ## Annotation functions
 annotate_obs_paucity <- function(data, col_spec){
   data %>%
-    group_by(sta6a) %>%
+    group_by(id) %>%
     arrange(report_month) %>%
     summarize(obs_paucity = eval_obs_paucity(documented, not_documented))
 }
 
 annotate_negative_gap <- function(data, col_spec){
-  data %>% group_by(sta6a) %>%
+  data %>% group_by(id) %>%
     dplyr::filter(report_month == max(report_month)) %>%
-    summarize(negative_gap = eval_gap(documented, not_documented))
+    summarize(negative_gap = eval_negative_gap(documented, not_documented))
 }
 
 annotate_negative_trend <- function(data, col_spec){
+  #cat(paste("\nnegTrend\n"), file=stderr())
   data %>%
-    group_by(sta6a) %>%
+    group_by(id) %>%
     arrange(report_month) %>%
-    summarize(negative_trend = eval_negative_trend(performance))
+    summarize(negative_trend = eval_negative_trend(documented, not_documented))
 }
