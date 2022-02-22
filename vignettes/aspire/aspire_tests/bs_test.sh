d63a2ba34236357080a219f94dfb43ac78a123ea
mradamgbeck <mradamgbeck@gmail.com>
2022-02-22 16:28:41 -0500

    cleanup

--- vignettes/aspire/aspire_tests/bs_test.sh
+++ vignettes/aspire/aspire_tests/bs_test.sh
@@ -22,12 +22,9 @@ done
 # Check if FUSEKI is running.
 FUSEKI_PING=$(curl -s -o /dev/null -w "%{http_code}" localhost:3030/$/ping)
 if [[ -z ${FUSEKI_PING}} || ${FUSEKI_PING} -ne 200 ]]; then
-  # Error
-  echo >&2 "Fuseki not running locally."
-
   # Try to start custom fuseki locally, will temporarily create a run/ directory
   ${FUSEKI_HOME}/fuseki-server --mem --update /ds 1>fuseki.out 2>&1 &
-  read -p "Waiting 5 seconds for Fuseki to start..." -t 5
+  read -t 5
 fi
 
 # Create ${OUTPUT_DIR} directory
@@ -37,33 +34,25 @@ mkdir -p ${OUTPUT_DIR}
 date >${LOG_FILE}
 
 #run bitstomach on each csv file in the TEST_DATA_DIR directory, and output speks / logs
-for file in ${TEST_DATA_DIR}/*.csv;
-  do
-    test_name=$(basename "${file}" .csv)
-    printf "%-${COL_WIDTH}s" "BitStomach - Aspire - ${test_name}" | tee -a ${LOG_FILE}
-    printf "\n" >>${LOG_FILE}
-    $DISPLAY_LAB_HOME/bit-stomach/bin/bitstomach.sh \
-      -s ${KNOWLEDGE_BASE_DIR}/spek.json \
-      -d ${file} \
-      -a ${KNOWLEDGE_BASE_DIR}/annotations.r \
-      2>>${LOG_FILE} | jq . \
-      > ${OUTPUT_DIR}/${test_name}.spek.json
-    printf "\nexit status: %d\n" "${?}"
+for file in ${TEST_DATA_DIR}/*.csv; do
+  echo "begin test --------------------------------------------------------" | tee -a ${LOG_FILE}
+  test_name=$(basename "${file}" .csv)
+  echo "BitStomach - Aspire - ${test_name}" | tee -a ${LOG_FILE}
+  $DISPLAY_LAB_HOME/bit-stomach/bin/bitstomach.sh \
+    -s ${KNOWLEDGE_BASE_DIR}/spek.json \
+    -d ${file} \
+    -a ${KNOWLEDGE_BASE_DIR}/annotations.r \
+    2>>${LOG_FILE} | jq . \
+    >${OUTPUT_DIR}/${test_name}.spek.json
+  echo "exit status: ${?}" | tee -a ${LOG_FILE}
 
-    $VR_SCRIPTS_DIR/summarize_graph.sh \
-      -s ${OUTPUT_DIR}/${test_name}.spek.json
-      2>>${LOG_FILE} | jq . \
-      > ${OUTPUT_DIR}/${test_name}.summary.txt
-  done
-
+  $VR_SCRIPTS_DIR/summarize_graph.sh \
+    -s ${OUTPUT_DIR}/${test_name}.spek.json \
+    >>${LOG_FILE} 2>&1
+  echo "end test   --------------------------------------------------------" | tee -a ${LOG_FILE}
+done
 
 # Run cleanup if debug is not enabled
 if [[ -z ${DEBUG_MODE} ]]; then
-  printf "%-${COL_WIDTH}s" "Running Cleanup Script" | tee -a ${LOG_FILE}
-  printf "\n" >>${LOG_FILE}
-  $DISPLAY_LAB_HOME/vert-ramp-affirmation/scripts/cleanup.sh >>${LOG_FILE} 2>&1
-  printf "exit status: %d\n" "${?}"
+  ./test_cleanup.sh
 fi
-
-
-printf "Log written to ${LOG_FILE}\n"
